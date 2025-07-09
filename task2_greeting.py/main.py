316f4afa454c5d6f459acb00e0af561e0f3c95d3
Abhishek45749 <abhisheksuryawanshi361@gmail.com>
2025-07-09 20:22:36 +0530

    Delete task2_greeting.py directory

--- task2_greeting.py/main.py
+++
@@ -1,9 +0,0 @@
-# Task 2: Personalized Greeting
-#Input from user
-A=input("Enter you First name:")
-B=input("Enter you last name:")
-#Combine names
-full_name=A+" "+B
-#Display greeting
-print(f"Hello {full_name}! Welcome to Python Programming")
-
